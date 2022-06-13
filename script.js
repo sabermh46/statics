e66062f8484c341eaacccf8176f30c1229b8b9fa
Saber Mahmud Sourav <74147777+sabermh46@users.noreply.github.com>
2022-06-13 06:32:49 +0600

    Update

--- script.js
+++ script.js
@@ -2,15 +2,29 @@
 
 
 
-var stats = document.querySelectorAll('.stat-body');
+var stats = document.querySelectorAll('.statics-container');
 
 stats.forEach((stat, idx)=>{
-    var items = document.querySelectorAll(`.stat-body:nth-child(${idx+1}) > .item`);
-    var max=0, min=0, value=[];
+    var items = document.querySelectorAll(`.statics-container:nth-child(${idx+1}) .stat-body > .item`);
+    var body = document.querySelector(`.statics-container:nth-child(${idx+1}) > .stat-body`);
+    var max=0, min=0, value=[], hFactor=1;
+    var bodyheight = body.offsetHeight;
+    
     items.forEach((item, i)=>{
         value[i] = parseFloat(item.textContent);
-        item.style.height = `${ height * 3}px`;
+    })
+
+    max = Math.max(...value);
+    min = Math.min(...value);
+    //console.log("Max = " + max + ",  Min = " + min);
+
+    bodyheight -= 0.1*bodyheight;
+    hFactor = bodyheight/max;
+
+    items.forEach((item, i)=>{
+        value[i] = parseFloat(item.textContent);
+        var initial = value[i];
+        item.style.height = `${ initial * hFactor }px`;
         item.style.transitionDelay = `${i*0.1}s`;
     })
-    console.log(value);
 })
